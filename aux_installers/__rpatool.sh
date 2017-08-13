b6dc08e547362bde0cd0aa09d198ca787a12146f
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-08-13 17:26:52 +0200

    Added 'rpatool', with custom scripts for installing and uninstalling

---
+++ aux_installers/__rpatool.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FILENAME="rpatool"
+
+URL="https://raw.githubusercontent.com/Shizmob/rpatool/master/$FILENAME"
+DESTINATION="/usr/local/bin/$FILENAME"
+
+
+code="$(curl -# -w "%{http_code}" "$URL" -L -o "$DESTINATION")"
+
+# Checks curl's return code and the servers' answer
+if [ $? -eq 0 ] && [ "$code" = "200" ]
+then
+	sudo chmod +x "$DESTINATION"
+	return 0
+else
+	return "$code"
+fi
---
+++ aux_uninstallers/__rpatool.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+FILENAME="rpatool"
+
+DESTINATION="/usr/local/bin/$FILENAME"
+
+rm -fv "$DESTINATION"
