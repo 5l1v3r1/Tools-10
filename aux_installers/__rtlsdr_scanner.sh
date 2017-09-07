9140ac8b786c225f9b0017d3f8bab3482c4e7728
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-09-07 14:53:06 +0200

    Fixed 'rtlsdr_scanner' scripts

--- aux_installers/__rtlsdr_scanner.sh
+++ aux_installers/__rtlsdr_scanner.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
 # https://eartoearoak.com/software/rtlsdr-scanner/rtlsdr-scanner-installation
+DEST_BIN="/usr/local/bin/rtlsdr_scanner"
+
 
 # Requirements
 sudo apt-get install python python-wxgtk3.0 rtl-sdr
 
 # Installation
 sudo -H pip install -U rtlsdr_scanner
+res="$?"
+
+# Adds a little script to run the
+code="#!/bin/sh
+python -m rtlsdr_scanner
+"
+
+printf "Creating file '%s' with the following code: " "$DEST_BIN"
+printf "%s\n" "$code" | sudo tee "$DEST_BIN"
+
+if [ "$?" -eq 0 ]
+then
+	printf "Done"
+else
+	printf " --> Error creating file '%s' <-- " "$DEST_BIN"
+	return "1"
+fi
+
+return "$res"
--- aux_uninstallers/__rtlsdr_scanner.sh
+++ aux_uninstallers/__rtlsdr_scanner.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 # https://eartoearoak.com/software/rtlsdr-scanner/rtlsdr-scanner-installation
+DEST_BIN="/usr/local/bin/rtlsdr_scanner"
 
-sudo -H pip uninstall -U rtlsdr_scanner
+sudo -H pip uninstall rtlsdr_scanner
+res="$?"
+
+# Removes the binary
+if [ -f "$DEST_BIN" ]
+then
+	sudo rm -v "$DEST_BIN"
+fi
+
+return "$res"
